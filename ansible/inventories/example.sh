1f24e6397fbc468ec200db36f0ba0e5201fcff45
Dan Sullivan <dansullivan@gmail.com>
2025-12-14 18:42:56 -0500

    docs: update comment

--- ansible/inventories/example.sh
+++ ansible/inventories/example.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# Dynamic inventory script that gets the server IP from Terraform output
+# Dynamic inventory script that gets the server hostname from Terraform output
 set -e
 
 cd "$(dirname "$0")/../../terraform/environments/example"
