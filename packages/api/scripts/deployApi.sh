08108277ff1c048e67a354163bb97e458d1eeac9
Dan Sullivan <dansullivan@gmail.com>
2025-12-15 10:47:55 -0500

    fix: pr feedback

--- packages/api/scripts/deployApi.sh
+++ packages/api/scripts/deployApi.sh
@@ -12,6 +12,11 @@ if [ -z "$HOSTNAME" ]; then
   exit 1
 fi
 
+if [ -z "$USERNAME" ]; then
+  echo "Error: Could not get server_username from Terraform output"
+  exit 1
+fi
+
 # Build shared package (API dependency)
 pnpm --filter @rapid/shared build
 
