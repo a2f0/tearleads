113f7be7056a13a8c11bcc771bb9c8dffc555585
Dan Sullivan <dansullivan@gmail.com>
2025-12-15 09:15:11 -0500

    feat: website

---
+++ packages/ui/src/index.ts
@@ -0,0 +1,16 @@
+// Components
+export { Footer, type FooterProps } from './components/footer.js';
+export {
+  ThemeSwitcher,
+  type ThemeSwitcherProps
+} from './components/themeSwitcher.js';
+
+// Context
+export {
+  ThemeProvider,
+  type ThemeProviderProps,
+  useTheme
+} from './context/themeProvider.js';
+
+// Utilities
+export { cn } from './lib/utils.js';
