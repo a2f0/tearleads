5541f4aac71f8307492e1e22315df0a3194fa000
Dan Sullivan <dansullivan@gmail.com>
2025-12-19 19:43:01 -0500

    feat: add error boundary and lazyily evaluate api url

--- packages/client/src/main.tsx
+++ packages/client/src/main.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import App from './App';
+import { ErrorBoundary } from './components/ui/error-boundary';
 import { Settings } from './pages/Settings';
 import './index.css';
 
@@ -10,14 +11,16 @@ const rootElement = document.getElementById('root');
 if (rootElement) {
   ReactDOM.createRoot(rootElement).render(
     <React.StrictMode>
-      <ThemeProvider>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<App />} />
-            <Route path="/settings" element={<Settings />} />
-          </Routes>
-        </BrowserRouter>
-      </ThemeProvider>
+      <ErrorBoundary>
+        <ThemeProvider>
+          <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<App />} />
+              <Route path="/settings" element={<Settings />} />
+            </Routes>
+          </BrowserRouter>
+        </ThemeProvider>
+      </ErrorBoundary>
     </React.StrictMode>
   );
 }
