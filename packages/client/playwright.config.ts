5b1994d7e85a25c7c8988e00543957dc51bbd316
Dan Sullivan <dansullivan@gmail.com>
2025-12-20 18:47:36 -0500

    fix: pr feedback and align retry configuration

--- packages/client/playwright.config.ts
+++ packages/client/playwright.config.ts
@@ -9,12 +9,11 @@ export default defineConfig({
   testIgnore: ['**/electron/**'],
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
-  retries: process.env.CI ? 2 : 0,
+  retries: 0,
   workers: process.env.CI ? 1 : undefined,
   reporter: 'html',
   use: {
     baseURL: 'http://localhost:3000',
-    trace: 'on-first-retry',
   },
 
   projects: [
