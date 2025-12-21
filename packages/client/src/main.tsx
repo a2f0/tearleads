8ec3cd3e1826031dacadddd4d4eb84a286f3b728
Dan Sullivan <dansullivan@gmail.com>
2025-12-21 11:05:49 -0500

    refactor: convert debug modal to route

--- packages/client/src/main.tsx
+++ packages/client/src/main.tsx
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import App from './App';
 import { ErrorBoundary } from './components/ui/error-boundary';
+import { Debug } from './pages/Debug';
 import { Settings } from './pages/Settings';
 import './index.css';
 
@@ -16,6 +17,7 @@ if (rootElement) {
           <BrowserRouter>
             <Routes>
               <Route path="/" element={<App />} />
+              <Route path="/debug" element={<Debug />} />
               <Route path="/settings" element={<Settings />} />
             </Routes>
           </BrowserRouter>
