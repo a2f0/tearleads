1f9dfc7b1ee0c47bdb57297739fdde530e202ac2
Dan Sullivan <dansullivan@gmail.com>
2026-01-09 10:06:24 -0500

    fix: website build race condition and neovim hot reload
    - Add @rapid/api as website dependency to ensure build order
    - Add timer-based file change detection for neovim in screen/tmux
    - Add <leader>r keybinding to reload neovim config

--- scripts/config/neovim.lua
+++ scripts/config/neovim.lua
@@ -5,6 +5,9 @@ vim.opt.shadafile = "NONE"
 
 -- Auto-reload files when changed externally (e.g., git pull)
 vim.opt.autoread = true
+vim.opt.updatetime = 1000  -- Check for changes every 1 second of idle
+
+-- Check for file changes on various events (FocusGained may not fire in screen/tmux)
 vim.api.nvim_create_autocmd({"FocusGained", "BufEnter", "CursorHold", "CursorHoldI"}, {
   pattern = "*",
   callback = function()
@@ -13,6 +16,32 @@ vim.api.nvim_create_autocmd({"FocusGained", "BufEnter", "CursorHold", "CursorHol
     end
   end,
 })
+
+-- Timer-based file change detection (for screen/tmux where FocusGained doesn't fire)
+local timer = vim.uv.new_timer()
+if timer then
+  timer:start(2000, 2000, vim.schedule_wrap(function()
+    if vim.fn.mode() ~= 'c' and vim.bo.buftype == '' then
+      vim.cmd('silent! checktime')
+    end
+  end))
+end
+
+-- Keybinding to reload this config file: <leader>r
+vim.keymap.set('n', '<leader>r', function()
+  local config_path = vim.env.MYVIMRC or vim.fn.stdpath('config') .. '/init.lua'
+  -- Try to source the file that was used to start nvim
+  local init_file = vim.v.argv[3]  -- Usually the -u argument
+  if init_file and vim.fn.filereadable(init_file) == 1 then
+    vim.cmd('source ' .. init_file)
+    print('Reloaded: ' .. init_file)
+  elseif vim.fn.filereadable(config_path) == 1 then
+    vim.cmd('source ' .. config_path)
+    print('Reloaded: ' .. config_path)
+  else
+    print('Could not find config to reload')
+  end
+end, { desc = 'Reload neovim config' })
 
 -- disable builtin file browser
 vim.g.loaded_netrw = 1
