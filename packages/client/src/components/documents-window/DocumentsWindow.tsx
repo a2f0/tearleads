2598274140e52b4878bc8cd6e6e864cfd7f72035
Dan Sullivan <dansullivan@gmail.com>
2026-01-17 23:28:33 -0500

    feat(window): add documents window

---
+++ packages/client/src/components/documents-window/DocumentsWindow.tsx
@@ -0,0 +1,41 @@
+import type { WindowDimensions } from '@/components/floating-window';
+import { FloatingWindow } from '@/components/floating-window';
+import { Documents } from '@/pages/Documents';
+
+interface DocumentsWindowProps {
+  id: string;
+  onClose: () => void;
+  onMinimize: (dimensions: WindowDimensions) => void;
+  onFocus: () => void;
+  zIndex: number;
+  initialDimensions?: WindowDimensions;
+}
+
+export function DocumentsWindow({
+  id,
+  onClose,
+  onMinimize,
+  onFocus,
+  zIndex,
+  initialDimensions
+}: DocumentsWindowProps) {
+  return (
+    <FloatingWindow
+      id={id}
+      title="Documents"
+      onClose={onClose}
+      onMinimize={onMinimize}
+      onFocus={onFocus}
+      zIndex={zIndex}
+      {...(initialDimensions && { initialDimensions })}
+      defaultWidth={700}
+      defaultHeight={550}
+      minWidth={400}
+      minHeight={350}
+    >
+      <div className="flex h-full flex-col overflow-hidden p-3">
+        <Documents />
+      </div>
+    </FloatingWindow>
+  );
+}
