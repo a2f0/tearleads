c141bfa6b472e807a33ead52092fab312d632b70
Dan Sullivan <dansullivan@gmail.com>
2026-01-18 10:38:58 -0500

    chore(opfs): fix lint

--- packages/client/src/components/opfs-window/OpfsWindow.test.tsx
+++ packages/client/src/components/opfs-window/OpfsWindow.test.tsx
@@ -1,6 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import React from 'react';
+import type React from 'react';
 import { beforeEach, describe, expect, it, vi } from 'vitest';
 import { OpfsWindow } from './OpfsWindow';
 
