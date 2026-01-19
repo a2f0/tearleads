62afa88eadf294564cc810a68e2ee86ccad9d91d
Dan Sullivan <dansullivan@gmail.com>
2026-01-19 02:36:10 -0500

    style(sqlite): format table rows

--- packages/client/src/pages/TableRows.tsx
+++ packages/client/src/pages/TableRows.tsx
@@ -8,7 +8,9 @@ export function TableRows() {
   return (
     <TableRowsView
       tableName={tableName ?? null}
-      backLink={<BackLink defaultTo="/sqlite/tables" defaultLabel="Back to Tables" />}
+      backLink={
+        <BackLink defaultTo="/sqlite/tables" defaultLabel="Back to Tables" />
+      }
     />
   );
 }
