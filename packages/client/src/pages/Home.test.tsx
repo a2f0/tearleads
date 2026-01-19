6f2ff8a2d6985bce44c0cd5aff6378a948f158c8
Dan Sullivan <dansullivan@gmail.com>
2026-01-19 02:30:38 -0500

    chore(release): bump versions

--- packages/client/src/pages/Home.test.tsx
+++ packages/client/src/pages/Home.test.tsx
@@ -1,6 +1,6 @@
+import { ThemeProvider } from '@rapid/ui';
 import { act, fireEvent, render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import { ThemeProvider } from '@rapid/ui';
 import { MemoryRouter } from 'react-router-dom';
 import { afterEach, beforeEach, describe, expect, it, vi } from 'vitest';
 import { WindowManagerProvider } from '@/contexts/WindowManagerContext';
