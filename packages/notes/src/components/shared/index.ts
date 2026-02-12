1b5b74b34fa189c714bf7ddab3d6b71c2540c13c
Dan Sullivan <dansullivan@gmail.com>
2026-02-11 21:18:03 -0500

    refactor(notes): polish shared notes extraction

---
+++ packages/notes/src/components/shared/index.ts
@@ -0,0 +1,6 @@
+export { DatabaseLoadingCard } from './DatabaseLoadingCard';
+export { NotesContextMenus } from './NotesContextMenus';
+export { NotesEmptyStateCard } from './NotesEmptyStateCard';
+export { NotesLoadingCard } from './NotesLoadingCard';
+export { NotesViewHeader } from './NotesViewHeader';
+export { useCreateNote } from './useCreateNote';
