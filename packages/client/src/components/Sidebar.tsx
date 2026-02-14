7698c1909f1e5135245c5ff0b3b9a23847d431f3
Dan Sullivan <dansullivan@gmail.com>
2026-02-13 23:09:51 -0500

    style(debug): format sidebar export

--- packages/client/src/components/Sidebar.tsx
+++ packages/client/src/components/Sidebar.tsx
@@ -1,4 +1,8 @@
-export { adminFlyoutItems, debugFlyoutItems, navItems } from './sidebar/navItems';
+export {
+  adminFlyoutItems,
+  debugFlyoutItems,
+  navItems
+} from './sidebar/navItems';
 export { Sidebar } from './sidebar/Sidebar';
 export type {
   AdminFlyoutItem,
