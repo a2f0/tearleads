d6ba675639339b1e2ce302b1f8cccb9909bd89a8
Dan Sullivan <dansullivan@gmail.com>
2026-02-15 08:53:30 -0500

    refactor(terraform): remove legacy k8s and tee directories
    These have been migrated to terraform/stacks/{staging,prod}/{k8s,tee}

--- tee/ansible/scripts/buildApiImage.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-set -eu
-
-SCRIPT_DIR="$(CDPATH='' cd -- "$(dirname "$0")" && pwd)"
-ANSIBLE_ROOT="$(CDPATH='' cd -- "$SCRIPT_DIR/.." && pwd)"
-INVENTORY="${TEE_ANSIBLE_INVENTORY:-localhost,}"
-
-ansible-playbook -i "$INVENTORY" "$ANSIBLE_ROOT/playbooks/buildApiImage.yml" "$@"
--- tee/scripts/ansible-setup.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-set -eu
-
-"$(dirname "$0")"/../ansible/scripts/setup.sh "$@"
