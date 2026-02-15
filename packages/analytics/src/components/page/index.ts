52dcfd61ce647dcee2ef1ad761fc83ad3bbeb9bc
Dan Sullivan <dansullivan@gmail.com>
2026-02-15 00:35:10 -0500

    style(analytics): fix import organization order

--- packages/analytics/src/components/page/index.ts
+++ packages/analytics/src/components/page/index.ts
@@ -2,6 +2,6 @@ export { Analytics } from './Analytics';
 export { AnalyticsEventsPanel } from './AnalyticsEventsPanel';
 export { AnalyticsFiltersSummaryPanel } from './AnalyticsFiltersSummaryPanel';
 export { AnalyticsPageHeader } from './AnalyticsPageHeader';
-export { SortIcon } from './SortIcon';
 export type { SortState } from './SortIcon';
+export { SortIcon } from './SortIcon';
 export type { SummarySortState, TimeFilter } from './types';
