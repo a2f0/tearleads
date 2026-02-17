d0896b05990c4e2f81b69e004f333ca62f652287
Dan Sullivan <dansullivan@gmail.com>
2026-02-16 21:15:57 -0500

    chore(terraform): hydrate ssh host keys

--- terraform/stacks/prod/k8s/scripts/destroy.sh
+++ terraform/stacks/prod/k8s/scripts/destroy.sh
@@ -3,6 +3,12 @@ set -eu
 
 SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 STACK_DIR="$(dirname "$SCRIPT_DIR")"
+REPO_ROOT="$(git rev-parse --show-toplevel)"
+
+# shellcheck source=../../../../scripts/common.sh
+source "$REPO_ROOT/terraform/scripts/common.sh"
+
+setup_ssh_host_keys
 
 echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
 echo "!! WARNING: This will destroy the PRODUCTION k8s cluster.   !!"
--- terraform/stacks/staging/dev-server/scripts/destroy.sh
+++ terraform/stacks/staging/dev-server/scripts/destroy.sh
@@ -3,6 +3,12 @@ set -eu
 
 SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 STACK_DIR="$(dirname "$SCRIPT_DIR")"
+REPO_ROOT="$(git rev-parse --show-toplevel)"
+
+# shellcheck source=../../../../scripts/common.sh
+source "$REPO_ROOT/terraform/scripts/common.sh"
+
+setup_ssh_host_keys
 
 echo "WARNING: This will destroy the staging dev-server infrastructure."
 echo "Press Ctrl+C to cancel, or wait 5 seconds to continue..."
