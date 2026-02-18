88da602f05a14243af495b0a7cbabe8cd97aa8c2
Dan Sullivan <dansullivan@gmail.com>
2026-02-18 01:57:27 -0500

    fix(knip): restore admin package files

---
+++ packages/admin/src/components/admin-postgres/index.ts
@@ -0,0 +1,2 @@
+export { PostgresConnectionPanel } from './PostgresConnectionPanel';
+export { PostgresTableSizes } from './PostgresTableSizes';
---
+++ packages/admin/src/components/admin-window/index.ts
@@ -0,0 +1,4 @@
+export { AdminGroupsWindow } from './AdminGroupsWindow';
+export { AdminPostgresWindow } from './AdminPostgresWindow';
+export { AdminRedisWindow } from './AdminRedisWindow';
+export { AdminWindow } from './AdminWindow';
