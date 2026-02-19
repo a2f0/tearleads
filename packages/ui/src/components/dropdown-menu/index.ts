0f972e67aa4ed9dea10703d2dd2c5db23a5c5943
Dan Sullivan <dansullivan@gmail.com>
2026-02-19 07:15:57 -0500

    style(ui): format dropdown barrel

--- packages/ui/src/components/dropdown-menu/index.ts
+++ packages/ui/src/components/dropdown-menu/index.ts
@@ -1,3 +1,6 @@
 export { DropdownMenu, useDropdownMenuContext } from './DropdownMenu.js';
-export { DropdownMenuItem, type DropdownMenuItemProps } from './DropdownMenuItem.js';
+export {
+  DropdownMenuItem,
+  type DropdownMenuItemProps
+} from './DropdownMenuItem.js';
 export { DropdownMenuSeparator } from './DropdownMenuSeparator.js';
