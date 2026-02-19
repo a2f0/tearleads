e6503fd4b5cc3a681c2883aaf29fdf6b75deacbb
Dan Sullivan <dansullivan@gmail.com>
2026-02-19 08:01:36 -0500

    feat(vfs): add onboarding register flow

--- packages/api-client/src/vfsCrypto/index.ts
+++ packages/api-client/src/vfsCrypto/index.ts
@@ -1,4 +1,6 @@
 export * from './keyOnboarding';
+export * from './onboardingClient';
+export * from './registrationClient';
 export type * from './engine';
 export type * from './keyManager';
 export type * from './secureWritePipeline';
---
+++ packages/api-client/src/vfsCrypto/registrationClient.ts
@@ -0,0 +1,75 @@
+import {
+  combineEncapsulation,
+  deserializePublicKey,
+  wrapKeyForRecipient,
+  type VfsObjectType,
+  type VfsRegisterResponse
+} from '@tearleads/shared';
+import { api } from '../apiClient';
+import {
+  ensureVfsOnboardingKeys,
+  type VfsOnboardingApiClient
+} from './onboardingClient';
+
+export interface RegisterVfsItemWithOnboardingInput {
+  password: string;
+  id: string;
+  objectType: VfsObjectType;
+  apiClient: VfsOnboardingApiClient & {
+    register(data: {
+      id: string;
+      objectType: VfsObjectType;
+      encryptedSessionKey: string;
+    }): Promise<VfsRegisterResponse>;
+  };
+  sessionKey?: Uint8Array;
+}
+
+export interface RegisterVfsItemWithOnboardingResult {
+  sessionKey: Uint8Array;
+  registerResponse: VfsRegisterResponse;
+  createdKeys: boolean;
+}
+
+function createSessionKey(): Uint8Array {
+  return crypto.getRandomValues(new Uint8Array(32));
+}
+
+export async function registerVfsItemWithOnboarding(
+  input: RegisterVfsItemWithOnboardingInput
+): Promise<RegisterVfsItemWithOnboardingResult> {
+  const keyResult = await ensureVfsOnboardingKeys({
+    password: input.password,
+    apiClient: input.apiClient
+  });
+
+  const sessionKey = input.sessionKey ?? createSessionKey();
+  const recipientPublicKey = deserializePublicKey(keyResult.publicKey);
+  const wrappedSessionKey = combineEncapsulation(
+    wrapKeyForRecipient(sessionKey, recipientPublicKey)
+  );
+
+  const registerResponse = await input.apiClient.register({
+    id: input.id,
+    objectType: input.objectType,
+    encryptedSessionKey: wrappedSessionKey
+  });
+
+  return {
+    sessionKey,
+    registerResponse,
+    createdKeys: keyResult.created
+  };
+}
+
+export async function registerVfsItemWithApiOnboarding(input: {
+  password: string;
+  id: string;
+  objectType: VfsObjectType;
+  sessionKey?: Uint8Array;
+}): Promise<RegisterVfsItemWithOnboardingResult> {
+  return registerVfsItemWithOnboarding({
+    ...input,
+    apiClient: api.vfs
+  });
+}
