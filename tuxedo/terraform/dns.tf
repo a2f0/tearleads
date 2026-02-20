12c9fe47ef6818bc9b94f756d4c4d894091da5eb
Dan Sullivan <dansullivan@gmail.com>
2026-02-20 12:16:36 -0500

    fix(tuxedo): use Cloudflare DNS and staging_domain variable
    Fix terraform configuration to:
    - Use Cloudflare provider for DNS (matching existing state)
    - Add cloudflare_api_token and cloudflare_account_id variables
    - Rename domain variable to staging_domain for consistency
    - Update hcloud provider to ~> 1.58

--- tuxedo/terraform/dns.tf
+++ tuxedo/terraform/dns.tf
@@ -1,27 +1,26 @@
-data "hcloud_zone" "main" {
-  name = var.domain
+data "cloudflare_zone" "staging" {
+  account_id = var.cloudflare_account_id
+  name       = var.staging_domain
 }
 
 locals {
   ipv6_address = "${trimsuffix(hcloud_server.tuxedo.ipv6_network, "::/64")}::1"
 }
 
-resource "hcloud_zone_rrset" "tuxedo_ipv4" {
-  zone = data.hcloud_zone.main.name
-  name = var.dns_hostname
-  type = "A"
-  ttl  = 60
-  records = [
-    { value = hcloud_server.tuxedo.ipv4_address }
-  ]
+resource "cloudflare_record" "tuxedo_ipv4" {
+  zone_id = data.cloudflare_zone.staging.id
+  name    = var.dns_hostname
+  type    = "A"
+  content = hcloud_server.tuxedo.ipv4_address
+  proxied = false
+  ttl     = 60
 }
 
-resource "hcloud_zone_rrset" "tuxedo_ipv6" {
-  zone = data.hcloud_zone.main.name
-  name = var.dns_hostname
-  type = "AAAA"
-  ttl  = 60
-  records = [
-    { value = local.ipv6_address }
-  ]
+resource "cloudflare_record" "tuxedo_ipv6" {
+  zone_id = data.cloudflare_zone.staging.id
+  name    = var.dns_hostname
+  type    = "AAAA"
+  content = local.ipv6_address
+  proxied = false
+  ttl     = 60
 }
