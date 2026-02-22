e9deade9d924527e40b3a0c5ff5e31a68ff52d65
Dan Sullivan <dansullivan@gmail.com>
2026-02-22 02:09:24 -0500

    chore(knip): M36 remove unused type exports from client i18n

--- packages/client/src/i18n/index.ts
+++ packages/client/src/i18n/index.ts
@@ -1,10 +1,4 @@
-export type {
-  CommonKeys,
-  MenuKeys,
-  NamespaceKeys,
-  TranslationKeys,
-  Translations
-} from './hooks';
+export type { MenuKeys } from './hooks';
 export { useTranslation, useTypedTranslation } from './hooks';
 export type { SupportedLanguage } from './i18n';
 export { i18n, loadLanguage, supportedLanguages } from './i18n';
