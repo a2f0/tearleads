bbc02a79944f3e4bd872333ec064aac11458b14c
Dan Sullivan <dansullivan@gmail.com>
2026-03-09 21:26:23 -0400

    style(client): sort imports in main

--- packages/client/src/main.tsx
+++ packages/client/src/main.tsx
@@ -1,3 +1,4 @@
+import { AudioProvider } from '@tearleads/app-audio';
 import { configureBackupsRuntime } from '@tearleads/app-backups';
 import { setKeychainDependencies } from '@tearleads/app-keychain/clientEntry';
 import { ThemeProvider } from '@tearleads/ui';
@@ -7,7 +8,6 @@ import { I18nextProvider } from 'react-i18next';
 import { BrowserRouter } from 'react-router-dom';
 import { Toaster } from 'sonner';
 import { AppRoutes } from './AppRoutes';
-import { AudioProvider } from '@tearleads/app-audio';
 import { clientBackupsRuntime } from './backups/backupsRuntime';
 import { AppTooltipProvider } from './components/AppTooltipProvider';
 import { AuthInstanceBinding } from './components/AuthInstanceBinding';
