db6110b8c77750ced8e5c166dd733c101d5d4863
Dan Sullivan <dansullivan@gmail.com>
2026-03-09 21:20:55 -0400

    refactor(client): remove audio barrel

--- packages/client/src/audio/index.ts
+++
@@ -1,9 +0,0 @@
-// Re-export from @tearleads/app-audio package
-export {
-  AudioProvider,
-  type AudioTrack,
-  type RepeatMode,
-  useAudio,
-  useAudioAnalyser,
-  useAudioContext
-} from '@tearleads/app-audio';
