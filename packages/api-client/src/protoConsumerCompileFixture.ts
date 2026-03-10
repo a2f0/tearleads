8834326504207e94a74ef0196d1ea647dd75b236
Dan Sullivan <dansullivan@gmail.com>
2026-03-10 16:54:27 -0400

    style(api-client): sort proto fixture imports

--- packages/api-client/src/protoConsumerCompileFixture.ts
+++ packages/api-client/src/protoConsumerCompileFixture.ts
@@ -1,25 +1,25 @@
 import { createClient } from '@connectrpc/connect';
 import { createConnectTransport } from '@connectrpc/connect-web';
-import { AdminService } from '@tearleads/shared/gen/tearleads/v2/admin_pb';
-import { AiService } from '@tearleads/shared/gen/tearleads/v2/ai_pb';
-import { AuthService } from '@tearleads/shared/gen/tearleads/v2/auth_pb';
-import { BillingService } from '@tearleads/shared/gen/tearleads/v2/billing_pb';
-import { ChatService } from '@tearleads/shared/gen/tearleads/v2/chat_pb';
-import { MlsService } from '@tearleads/shared/gen/tearleads/v2/mls_pb';
-import { NotificationService } from '@tearleads/shared/gen/tearleads/v2/notifications_pb';
-import { RevenuecatService } from '@tearleads/shared/gen/tearleads/v2/revenuecat_pb';
-import { VfsService } from '@tearleads/shared/gen/tearleads/v2/vfs_pb';
-import { VfsSharesService } from '@tearleads/shared/gen/tearleads/v2/vfs_shares_pb';
 import * as adminConnect from '@tearleads/shared/gen/tearleads/v2/admin_connect';
+import { AdminService } from '@tearleads/shared/gen/tearleads/v2/admin_pb';
 import * as aiConnect from '@tearleads/shared/gen/tearleads/v2/ai_connect';
+import { AiService } from '@tearleads/shared/gen/tearleads/v2/ai_pb';
 import * as authConnect from '@tearleads/shared/gen/tearleads/v2/auth_connect';
+import { AuthService } from '@tearleads/shared/gen/tearleads/v2/auth_pb';
 import * as billingConnect from '@tearleads/shared/gen/tearleads/v2/billing_connect';
+import { BillingService } from '@tearleads/shared/gen/tearleads/v2/billing_pb';
 import * as chatConnect from '@tearleads/shared/gen/tearleads/v2/chat_connect';
+import { ChatService } from '@tearleads/shared/gen/tearleads/v2/chat_pb';
 import * as mlsConnect from '@tearleads/shared/gen/tearleads/v2/mls_connect';
+import { MlsService } from '@tearleads/shared/gen/tearleads/v2/mls_pb';
 import * as notificationsConnect from '@tearleads/shared/gen/tearleads/v2/notifications_connect';
+import { NotificationService } from '@tearleads/shared/gen/tearleads/v2/notifications_pb';
 import * as revenuecatConnect from '@tearleads/shared/gen/tearleads/v2/revenuecat_connect';
+import { RevenuecatService } from '@tearleads/shared/gen/tearleads/v2/revenuecat_pb';
 import * as vfsConnect from '@tearleads/shared/gen/tearleads/v2/vfs_connect';
+import { VfsService } from '@tearleads/shared/gen/tearleads/v2/vfs_pb';
 import * as vfsSharesConnect from '@tearleads/shared/gen/tearleads/v2/vfs_shares_connect';
+import { VfsSharesService } from '@tearleads/shared/gen/tearleads/v2/vfs_shares_pb';
 
 const transport = createConnectTransport({ baseUrl: 'https://example.test' });
 
