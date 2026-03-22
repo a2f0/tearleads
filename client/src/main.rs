56f4ab6ee43ff7830040b130f5b486c659576aee
Dan Sullivan <dansullivan@gmail.com>
2026-03-21 22:36:00 -0400

    cleanup: unneeded main

--- client/src/main.rs
+++
@@ -1,9 +0,0 @@
-use transport::tcp::TcpTransport;
-
-fn main() -> std::io::Result<()> {
-    let transport = TcpTransport::connect(protocol::DEFAULT_ADDR)?;
-    let mut client = client::Client::new(transport);
-    client.send(b"Hello from client!")?;
-    println!("Request sent to {}", protocol::DEFAULT_ADDR);
-    Ok(())
-}
