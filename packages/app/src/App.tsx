3a26d60673541006f236ce6e90669735ad5fbf51
Dan Sullivan <dansullivan@gmail.com>
2026-03-23 14:23:13 -0400

    refactor: extract layout

--- packages/app/src/App.tsx
+++ packages/app/src/App.tsx
@@ -1,37 +1,10 @@
-import { useCallback, useState } from "react";
-import { Pane } from "./components/Pane";
-import { DatabaseProvider } from "./db/DatabaseProvider";
 import { createAppDatabaseWorker } from "./db/sqliteWorker";
-import { Footer } from "./Footer";
-import "./App.css";
+import { Layout } from "./Layout";
 
 interface AppProps {
   createWorker?: typeof createAppDatabaseWorker;
 }
 
-function AppContent({ createWorker }: Required<AppProps>) {
-  const [split, setSplit] = useState(true);
-
-  const toggleSplit = useCallback(() => setSplit((s) => !s), []);
-
-  return (
-    <div className={split ? "layout layout--split" : "layout"}>
-      <header>
-        <button type="button" onClick={toggleSplit}>
-          {split ? "Unsplit" : "Split"}
-        </button>
-      </header>
-      <DatabaseProvider createWorker={createWorker}>
-        <Pane className="pane pane-left" />
-      </DatabaseProvider>
-      <DatabaseProvider createWorker={createWorker}>
-        <Pane className="pane pane-right" />
-      </DatabaseProvider>
-      <Footer />
-    </div>
-  );
-}
-
 export function App({ createWorker = createAppDatabaseWorker }: AppProps) {
-  return <AppContent createWorker={createWorker} />;
+  return <Layout createWorker={createWorker} />;
 }
