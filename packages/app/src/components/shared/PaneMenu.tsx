40fd48a2ca3b21bd9e8552230351ba5ec6ebea0c
a2f0 <dansullivan@gmail.com>
2026-03-23 20:07:32 -0400

    feat: conditional pane menu (#36)

--- packages/app/src/components/shared/PaneMenu.tsx
+++ packages/app/src/components/shared/PaneMenu.tsx
@@ -9,24 +9,29 @@ export function PaneMenu({
   position: MenuPosition;
   onClose: () => void;
 }) {
-  const { killWorker, spawnWorker } = useDatabase();
+  const { killWorker, spawnWorker, status } = useDatabase();
+  const isTerminated = status === "terminated";
 
   return (
     <Menu position={position} onClose={onClose}>
-      <MenuItem
-        label="Kill Worker"
-        onClick={() => {
-          killWorker();
-          onClose();
-        }}
-      />
-      <MenuItem
-        label="Spawn Worker"
-        onClick={() => {
-          spawnWorker();
-          onClose();
-        }}
-      />
+      {!isTerminated && (
+        <MenuItem
+          label="Kill Worker"
+          onClick={() => {
+            killWorker();
+            onClose();
+          }}
+        />
+      )}
+      {isTerminated && (
+        <MenuItem
+          label="Spawn Worker"
+          onClick={() => {
+            spawnWorker();
+            onClose();
+          }}
+        />
+      )}
     </Menu>
   );
 }
