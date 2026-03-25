ae165736aa3f1efb48051ba171103888787010ba
Dan Sullivan <dansullivan@gmail.com>
2026-03-25 13:43:52 -0400

    feat: items table

--- packages/api/src/adapters/postgres.ts
+++ packages/api/src/adapters/postgres.ts
@@ -11,7 +11,14 @@ await client.exec(`
     fingerprint TEXT NOT NULL UNIQUE,
     public_key TEXT NOT NULL,
     created_at TIMESTAMP NOT NULL DEFAULT now()
-  )
+  );
+  CREATE TABLE IF NOT EXISTS items (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    payload TEXT NOT NULL,
+    encrypted_data TEXT NOT NULL,
+    spicedb_zed_token TEXT NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT now()
+  );
 `);
 
 export default client;
--- packages/api/src/schema.ts
+++ packages/api/src/schema.ts
@@ -6,3 +6,11 @@ export const users = pgTable("users", {
   publicKey: text("public_key").notNull(),
   createdAt: timestamp("created_at").defaultNow().notNull(),
 });
+
+export const items = pgTable("items", {
+  id: uuid("id").defaultRandom().primaryKey(),
+  payload: text("payload").notNull(),
+  encryptedData: text("encrypted_data").notNull(),
+  spicedbZedToken: text("spicedb_zed_token").notNull(),
+  createdAt: timestamp("created_at").defaultNow().notNull(),
+});
