0e1f4e90ba7865b7e0adb4607a06013efb414b54
Dan Sullivan <dansullivan@gmail.com>
2026-05-20 11:36:36 -0400

    fix: make document facade projector exports explicit

--- packages/client-sdk/src/documents.ts
+++ packages/client-sdk/src/documents.ts
@@ -2,4 +2,26 @@ export * from "./data/blobContracts";
 export * from "./data/documentSummary";
 export * from "./data/documents/documentConstants";
 export * from "./data/documents/documentContent";
-export * from "./data/documents/documentKinds";
+export {
+  createDocumentProjectorRegistry,
+  type DocumentFieldValidationIssue,
+  type DocumentProjection,
+  type DocumentProjectorDefinition,
+  type DocumentProjectorInput,
+  type DocumentProjectorRegistry,
+  defaultDocumentProjectorRegistry,
+  deriveStoredDocumentTitle,
+  getStoredDocumentTypeLabel,
+  getUntitledDocumentTitle,
+  initializeStoredDocumentKind,
+  projectStoredDocumentState,
+  readStoredDocumentState,
+  readStringDocumentField,
+  type StoredDocumentKind,
+  type StoredDocumentState,
+  type StructuredDocumentMap,
+  type StructuredDocumentShape,
+  type StructuredDocumentText,
+  type ValidatedDocumentFields,
+  writeStoredDocumentFields,
+} from "./data/documents/documentKinds";
