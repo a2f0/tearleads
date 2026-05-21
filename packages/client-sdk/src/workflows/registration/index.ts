20861555525d0d272c697822296e037933a935b9
a2f0 <dansullivan@gmail.com>
2026-05-21 04:33:31 -0400

    refactor: move registration workflow into client SDK (#693)

--- packages/client-sdk/src/workflows/registration/index.ts
+++ packages/client-sdk/src/workflows/registration/index.ts
@@ -1,6 +1,13 @@
 export { bootstrapRootContainer } from "./bootstrapRootContainer";
 export { persistRegistrationBootstrap } from "./persistRegistrationBootstrap";
 export {
+  buildInitialOrganizationPolicyRequest,
+  principalPolicyBundleFromInitialGroupRequest,
+  type RegisterIdentityInput,
+  type RegistrationApi,
+  registerIdentity,
+} from "./registerIdentity";
+export {
   createInitialRootMetadataBootstrap,
   type InitialRootMetadataBootstrap,
 } from "./rootMetadataBootstrap";
