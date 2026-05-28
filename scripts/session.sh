bf98c8d949da89e35b7e0ce895a0c1c263c13982
Dan Sullivan <dansullivan@gmail.com>
2026-05-28 08:08:32 -0400

    feat: website dev server script

--- scripts/session.sh
+++ scripts/session.sh
@@ -27,6 +27,7 @@ repo_script_paths="$repo_root/packages/api/scripts
 $repo_root/packages/app-web/scripts
 $repo_root/packages/app-electrobun/scripts
 $repo_root/packages/sqlite-instance/scripts
+$repo_root/packages/website/scripts
 $repo_root/scripts
 $repo_root/scripts/git
 $repo_root/scripts/testing"
