9996fc55f98b1a10a5d8c12cd6bbf873d6c3e178
Dan Sullivan <dansullivan@gmail.com>
2026-06-20 19:52:04 -0400

    fix: reject streamed multipart part bodies

--- packages/api-client/src/routes/blobs/multipartStage.ts
+++ packages/api-client/src/routes/blobs/multipartStage.ts
@@ -18,7 +18,7 @@ const BLOB_PART_UPLOAD_ID_HEADER = "X-Tearleads-Blob-Upload-Id";
 
 export interface UploadMultipartBlobPartBytesRequest {
   readonly byteLength: number;
-  readonly encryptedBytes: BodyInit;
+  readonly encryptedBytes: Blob | BufferSource;
   readonly sha256: string;
   readonly uploadId: string;
 }
